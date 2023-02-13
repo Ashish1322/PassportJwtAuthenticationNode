34616880b832900aa09bd6c8719729d06d83047a
Ashish1322 <69026067+Ashish1322@users.noreply.github.com>
2023-02-13 23:09:36 +0530

    Update myurl.js

--- setup/myurl.js
+++ setup/myurl.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURL: "mongodb+srv://ashish1322:Ashish123.@cluster0.9h5agcr.mongodb.net/test",
-    secret: "ilovenishthajain"
+    mongoURL: "*********",
+    secret: "*********"
 
 }
